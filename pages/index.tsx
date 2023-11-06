a1b4346e70e09dd40080c1076caa2df4ecc14b6f
Vishav <vishavgarg96@gmail.com>
2023-11-06 12:23:57 +0530

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+
+const Home = () => {
+  const router = useRouter();
+  const [name, setName] = useState("");
+  const [today, setToday] = useState("");
+
+  useEffect(() => {
+    const userData = localStorage.getItem("user");
+    if (!userData) {
+      router.push("/signin");
+    }
+    if (typeof window !== "undefined") {
+      const user = JSON.parse(window.localStorage.getItem("user") || "{}");
+      setName(user ? user.name : "");
+
+      const now = new Date();
+      const options = { weekday: "long" };
+      setToday(now.toLocaleDateString(undefined, options));
+    }
+  }, [router]);
+
+  const handleLogout = () => {
+    localStorage.removeItem("user");
+    router.push("/signin");
+  };
+
+  return (
+    <div>
+      <nav className="bg-blue-500 p-4 text-white">
+        <div className="container mx-auto">
+          <div className="flex justify-between items-center">
+            <h1 className="text-2xl"></h1>
+            <button onClick={handleLogout}>Logout</button>
+          </div>
+        </div>
+      </nav>
+      <div className="container mx-auto mt-4">
+        <h2 className="text-2xl">Hi, {name}</h2>
+        <p>Happy {today}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ pages/signup.tsx
@@ -0,0 +1,89 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import { useRouter } from "next/router";
+import api from "../utils/api";
+
+const SignupSchema = yup.object().shape({
+  email: yup.string().email("Invalid email").required("Email is required"),
+  name: yup.string().required("Name is required"),
+  password: yup
+    .string()
+    .matches(
+      /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/,
+      "Password must meet requirements: 8 character, atleast 1 letter, 1 number and 1 special character"
+    )
+    .required("Password is required"),
+});
+
+const Signup = () => {
+  const router = useRouter();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(SignupSchema),
+  });
+
+  const onSubmit = async (data: any) => {
+    try {
+      const userData = await api.post("/user/signup", data);
+      localStorage.setItem("user", JSON.stringify(userData.data));
+      router.push("/");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-200">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="w-full max-w-sm bg-white p-8 rounded shadow-md"
+      >
+        <h2 className="text-2xl mb-4">Sign Up</h2>
+        <div className="mb-4">
+          <label>Email</label>
+          <input
+            type="text"
+            className="w-full border rounded p-2"
+            {...register("email")}
+          />
+          {errors.email && (
+            <p className="text-red-600">{errors.email.message}</p>
+          )}
+        </div>
+        <div className="mb-4">
+          <label>Name</label>
+          <input
+            type="text"
+            className="w-full border rounded p-2"
+            {...register("name")}
+          />
+          {errors.name && <p className="text-red-600">{errors.name.message}</p>}
+        </div>
+        <div className="mb-4">
+          <label>Password</label>
+          <input
+            type="password"
+            className="w-full border rounded p-2"
+            {...register("password")}
+          />
+          {errors.password && (
+            <p className="text-red-600">{errors.password.message}</p>
+          )}
+        </div>
+        <button
+          type="submit"
+          className="w-full bg-blue-600 text-white p-2 rounded hover:bg-blue-700"
+        >
+          Sign Up
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Signup;
